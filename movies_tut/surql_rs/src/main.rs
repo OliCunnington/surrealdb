4bae077e580410527d8ba81e01c3c355caa0c093
Oli <ofcunnington@hotmail.com>
2026-02-05 06:04:00 +0100

    querry from rust

--- movies_tut/surql_rs/src/main.rs
+++ movies_tut/surql_rs/src/main.rs
@@ -1,11 +1,40 @@
-use std::borrow::Cow;
+// use std::borrow::Cow;
 use serde::{Serialize, Deserialize};
 use serde_json::json;
 use surrealdb::{Error, Surreal};
 use surrealdb::opt::auth::Root;
 use surrealdb::engine::remote::ws::Ws;
 
-//structs...
+
+#[derive(Serialize, Deserialize)]
+struct Movie {
+    titel: String,
+    actors: Vec<String>,
+    awards: Option<String>,
+    box_office: Option<u32>,
+    directors: Vec<String>,
+    dvd_released: Option<String>,
+    genres: Vec<String>,
+    imdb_rating: Option<u32>,
+    languages: Vec<String>,
+    metacritic_rating: Option<u32>,
+    oscars_won: Option<u32>,
+    plot: String,
+    released: String,
+    rt_rating: Option<u32>,
+    runtime: u32,
+    writers: Vec<String>,
+    poster: Option<String>,
+    rated: Option<String>,
+    average_rating: f32,
+
+}
+
+#[derive(Serialize, Deserialize)]
+struct Person {
+    name: String,
+    roles: Vec<String>,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -20,4 +49,9 @@ async fn main() -> Result<(), Error> {
     // Select a specific namespace / database
     db.use_ns("main").use_db("main").await?;
 
+    let movies: Vec<Person> = db.select("movie").await?;
+
+    println!("{}", movies.len().to_string());
+
+    Ok(())
 }
